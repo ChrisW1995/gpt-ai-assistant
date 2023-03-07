b76d7b8178480478d6abd773b4c26d499262af2b
RD_Chris <chriswang6031@gmail.com>
2023-03-07 15:57:35 +0800

    Update event.js

--- app/models/event.js
+++ app/models/event.js
@@ -52,7 +52,7 @@ class Event {
    * @returns {boolean}
    */
     get isSpecificCommand() {
-      return this.message.text.toLowerCase().includes('@gpt')||this.message.text.includes('@friday');
+      return this.message.text.toLowerCase().includes('gpt')||this.message.text.includes('friday');
     }
   
   /**
